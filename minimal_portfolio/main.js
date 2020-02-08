4ea759d369af1d5de3b874114fec62f25f3341fc
Garrian BrownE <garrianbrown@gmail.com>
2020-02-07 19:14:35 -0600

    added resume

--- minimal_portfolio/main.js
+++ minimal_portfolio/main.js
@@ -1,6 +1,29 @@
-document.querySelector('.toggle').addEventListener('click', toggle);
+document.querySelector( '.toggle' ).addEventListener( 'click', toggle );
 
+typing();
+
+
+// Functions
 function toggle() {
-	let header = document.getElementById('header')
-	header.classList.toggle('active');
+	let header = document.getElementById( 'header' );
+	header.classList.toggle( 'active' );
 }
+
+// function typing() {
+// 	let i = 0;
+// 	let text = 'Engineer, Hobbyest, Gamer';
+// 	while ( i < text.length ) {
+// 		document.getElementById( 'title' ).innerHTML += text.charAt(i);
+// 		i++;
+// 		setTimeout(typing,500);
+// 	}break;
+// }
+
+
+// function typing() {
+// 	if ( i < text.length ) {
+// 		document.getElementById( 'title' ).innerHTML += text.charAt(i);
+// 		i++;
+// 		setTimeout(typing,50);
+// 	}
+// }
